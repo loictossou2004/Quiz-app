f6e98f008a89848cd5e402a028d88da8f5bbf853
loic <loic.tossou@epitech.eu>
2023-05-24 18:37:42 +0100

    All

--- src/components/Todo_components.js
+++ src/components/Todo_components.js
@@ -4,6 +4,9 @@ function Todolist() {
     const [currentquestion, setCurrentquestion] = useState(0);
     const [score, setScore] = useState(0)
     const [correctanswer, setCorrectAnswer] = useState(false)
+    const [Goodanswer, setGoodAnswer] = useState(0)
+    const [Finish, setFinish] = useState(false)
+
 
     const questions = [
       {
@@ -59,13 +62,24 @@ function Todolist() {
 
     const ClickOnOptions = async (goodanswer) => {
       if (!goodanswer)
-        setCorrectAnswer(true)
+      setCorrectAnswer(true)
       await delay(2000)
       setCorrectAnswer(false)
       await delay(500)
-      if (goodanswer)
+      if (goodanswer) {
         setScore(score + 20)
+        setGoodAnswer(Goodanswer + 1)
+      }
+      if (currentquestion === 4)
+        setFinish(true)
       setCurrentquestion(currentquestion + 1)
+    }
+
+    const Reinitialized = () => {
+      setScore(0)
+      setCurrentquestion(0)
+      setGoodAnswer(0)
+      setFinish(false)
     }
 
    
@@ -76,22 +90,38 @@ function Todolist() {
       <div className='max-w-[500PX] w-full mx-auto mt-4 bg-white p-8 rounded-3xl shadow-lm space-y-4'>
           <h1 className='text-center text-[#857E61] text-2xl font-bold py-4'>My Quiz App</h1>
           <div className='flex flex-row justify-center items-center space-x-5'>
-            <h1 className='text-center text-[#857E61] text-xl font-bold'>Your score is to {score}</h1>
           </div>
-          <ul className='space-y-3'>
+
+            { Finish === false ? 
+              <ul className='space-y-3'>
               <h2 className='text-center'>{questions[currentquestion].text}</h2>
               {questions[currentquestion].options.map(options => {
                   return (
                     <div>
                       <li key={options.id} onClick={() => ClickOnOptions(options.isCorrect)} className={options.isCorrect && correctanswer ? 'flex justify-between px-3 py-2 drop-shadow-lm rounded-lg cursor-pointer bg-green-400 '  : options.isCorrect ? 'flex justify-between bg-white px-3 py-2 drop-shadow-lm rounded-lg cursor-pointer active:bg-green-400 active:transition duration-500' : 'flex justify-between bg-white px-3 py-2 drop-shadow-lm rounded-lg cursor-pointer active:bg-red-600 active:transition duration-500'}
                       >
-                          <span /*className={options.isCorrect && correctanswer ? 'bg-' : null}*/>{options.text}</span>
+                          <span >{options.text}</span>
                       </li>
 
                     </div>
               )
               })}
           </ul>
+              :
+              <div className=''>
+                <p className='text-center text-xl font-bold'>Your score is <span className='text-[#857E61]'>{score}</span> and you got <span className='text-[#857E61]'>{Goodanswer}</span>/5</p>
+                <div className='flex justify-center py-3'>
+                  <button className='text-white bg-[#857E61] py-1 px-6 rounded-md' onClick={Reinitialized}>Reset</button>
+
+                </div>
+
+              </div>
+              
+            
+            }
+
+          
+          
       </div>
     </div>
   )
